1d65c90c3f3b200f2841a79be9eb189e7f2fca70
Simon Gustavsson <simongustavsson@driveworks.co.uk>
2015-07-27 09:23:12 +0100

    Initial commit of repro

---
+++ ClassLibrary1/Class1.vb
@@ -0,0 +1,13 @@
+﻿Imports System.Windows.Forms
+
+Public Class Class1
+
+    Public Sub Test(ByVal form As Form)
+        Console.WriteLine(form.Name)
+    End Sub
+
+    Public Sub Test(ByVal s As String)
+        Console.WriteLine(s)
+    End Sub
+
+End Class
---
+++ VS2015BrokeMyProject/Module1.vb
@@ -0,0 +1,12 @@
+﻿Imports ClassLibrary1
+
+Module Module1
+
+    Sub Main()
+        Dim c As New Class1()
+
+        ' Error BC30652	Reference required To assembly 'System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089' containing the type 'Form'. Add one to your project.	VS2015BrokeMyProject	VS2015BrokeMyProject\VS2015BrokeMyProject\Module1.vb	9
+        c.Test("hello")
+    End Sub
+
+End Module
